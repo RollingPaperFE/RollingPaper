ed2eb08bebbf658ba495924dc876a50cd8efadb5
aNN-algorithm <kyukyu0314@naver.com>
2025-07-14 16:14:14 +0900

    Feat: '메시지 보내기 페이지' 파일 생성

---
+++ src/SendMessagePage.jsx
@@ -0,0 +1,14 @@
+const SendMessagePage = () => {
+  return (
+    <div>
+      <div>From. 이름을 입력해 주세요.</div>
+      <div>프로필 이미지</div>
+      <div>상대와의 관계</div>
+      <div>내용을 입력해 주세요</div>
+      <div>폰트 선택</div>
+      <div>생성하기</div>
+    </div>
+  );
+};
+
+export default SendMessagePage;
