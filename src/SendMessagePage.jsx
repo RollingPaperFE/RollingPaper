07bb43abcc067478ec9740cf6e0d089d66fcbea4
aNN-algorithm <kyukyu0314@naver.com>
2025-07-14 16:25:26 +0900

    Feat: '메시지 보내기 페이지'에 styles 추가

--- src/SendMessagePage.jsx
+++ src/SendMessagePage.jsx
@@ -1,6 +1,8 @@
+import styles from "./SendMessagePage.module.css";
+
 const SendMessagePage = () => {
   return (
-    <div>
+    <div className={styles.SendMessagePage}>
       <div>From. 이름을 입력해 주세요.</div>
       <div>프로필 이미지</div>
       <div>상대와의 관계</div>
