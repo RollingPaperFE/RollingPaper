9f51806fd51d7487e7073dbfb12ca7336344e0c0
yejinshim <>
2025-07-15 22:37:53 +0900

    Design: 아이콘 헤더 CSS 추가

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,14 +1,41 @@
-import HeaderLogo from "./HeaderButton";
+import HeaderLogo from "./HeaderLogo";
 import HeaderButton from "./HeaderButton";
 import HeaderIconBar from "./HeaderIconBar";
 
+const writersGroup = [
+  { id: "a1", name: "Jennie", avatar: "url1" },
+  { id: "a2", name: "Rose", avatar: "url2" },
+  { id: "a3", name: "Jisoo", avatar: "url3" },
+  { id: "a4", name: "Lisa", avatar: "url4" },
+];
+
+const emojisGroup = [
+  { symbol: "👍", count: 24 },
+  { symbol: "😍", count: 16 },
+  { symbol: "🎉", count: 10 },
+];
+
+const addEmojiGroup = () => {
+  //이모지 추가
+};
+
+const shareGroup = () => {
+  //공유 기능
+};
+
 const Header = ({ type }) => {
   return (
-    <>
+    <header>
       {type === "Logo" && <HeaderLogo />}
       {type === "rolling" && <HeaderButton />}
-      <HeaderIconBar />
-    </>
+      <HeaderIconBar
+        name="Ashely Kim"
+        writers={writersGroup}
+        emojis={emojisGroup}
+        addEmoji={addEmojiGroup}
+        share={shareGroup}
+      />
+    </header>
   );
 };
 
--- src/Header/HeaderIconBar.jsx
+++ src/Header/HeaderIconBar.jsx
@@ -1,41 +1,66 @@
 import PropTypes from "prop-types";
+import AddEmoji from "../assets/header_add_img.png";
 import ShareImg from "../assets/header_share_img.png";
-
-//1. name 을 전달받으면 해당 이름이 표시되도록 설정
+import IconBarStyles from "./HeaderIconBar.module.css";
 
 const HeaderIconBar = ({ name, writers, emojis, addEmoji, share }) => {
   const sortedEmojis = [...emojis].sort((a, b) => b.count - a.count);
   return (
-    <div>
+    <div className={IconBarStyles.iconHeader}>
       {/* 1. 당사자 이름 */}
-      <div>
+      <div className={IconBarStyles.name}>
         <strong>To. {name}</strong>
       </div>
       {/* 2. 몇 명이 작성했는지 */}
-      <div>
-        {writers.slice(0, 3).map((user, idx) => (
-          <img key={idx} src={user.avatar} alt={user.name} className="avatar" />
-        ))}
-        {writers.length > 3 && <span>+{writers.length - 3}</span>}
-        <strong>{writers.length}명이 작성했어요!</strong>
-      </div>
-      {/* 3. 이모티콘 별 받은 수 표시 (내림차순)*/}
-      <div>
-        {sortedEmojis.map((emoji) => (
-          <span key={emoji.symbol}>
-            {emoji.symbol} {emoji.count}
-          </span>
-        ))}
-      </div>
-      {/* 4. 이모지 추가 기능 */}
-      <div>
-        <button onClick={addEmoji}>추가</button>
-      </div>
-      {/* 5. 공유 기능 */}
-      <div>
-        <button onClick={share}>
-          <img src={ShareImg} alt="공유하기" />
-        </button>
+      <div className={IconBarStyles.right}>
+        <div className={IconBarStyles.writers}>
+          <div className={IconBarStyles.writersAvatar}>
+            {writers.slice(0, 3).map(
+              (
+                user
+                // id // id를 설정해두었는데 왜 오류가 나는지 모르겠음
+              ) => (
+                <img
+                  key={user.id}
+                  src={user.avatar}
+                  alt={user.name}
+                  className="avatar"
+                />
+              )
+            )}
+            <div className={IconBarStyles.writersCount}>
+              {writers.length > 3 && (
+                <span className={IconBarStyles.writersPlus}>
+                  + {writers.length - 3}
+                </span>
+              )}
+              <span className={IconBarStyles.writersText}>
+                <strong>{writers.length}</strong>명이 작성했어요!
+              </span>
+            </div>
+          </div>
+        </div>
+        {/* 3. 이모티콘 별 받은 수 표시 (내림차순)*/}
+        <div className={IconBarStyles.emojis}>
+          {sortedEmojis.map((emoji) => (
+            <span key={emoji.symbol} className={IconBarStyles.emojiItems}>
+              {emoji.symbol} {emoji.count}
+            </span>
+          ))}
+        </div>
+        {/* 4. 이모지 추가 기능 */}
+        <div>
+          <button className={IconBarStyles.addEmoji} onClick={addEmoji}>
+            <img src={AddEmoji} alt="추가하기" />
+            추가
+          </button>
+        </div>
+        {/* 5. 공유 기능 */}
+        <div>
+          <button className={IconBarStyles.share} onClick={share}>
+            <img src={ShareImg} alt="공유하기" />
+          </button>
+        </div>
       </div>
     </div>
   );
