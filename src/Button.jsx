fe40103330d4ad6535fb8d57cab7e1d0bdac6ba1
limheewoo <heewoo39@gmail.com>
2025-07-16 09:09:01 +0900

    Refactor: pages & components로 분리

--- src/Button.jsx
+++
@@ -1,8 +0,0 @@
-import { Children } from "react";
-
-//className prop안에 css 모듈 넣어서 사용하시면 됩니다!
-const Button = ({ className, children }) => {
-  return <button className={className}>{children}</button>;
-};
-
-export default Button;
