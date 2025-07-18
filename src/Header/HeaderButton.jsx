5706a6da4f3cf93366c667872a6e7a8c8834c4d1
yejinshim <>
2025-07-18 18:19:06 +0900

    Feat: 로고,만들기 버튼 클릭 시 이동하는 링크 기능 추가

--- src/Header/HeaderButton.jsx
+++ src/Header/HeaderButton.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
 import globalStyle from "../../src/global.module.css";
 import headerButtonStyle from "./HeaderButton.module.css";
 import LogoImg from "../assets/LogoImg.png";
 import Button from "../assets/Button.jsx";
 
 const HeaderButton = () => {
+  // 1. 로그인 완료 - 롤링페이퍼 만들기 페이지 (post 경로로 이동) - 완료
+  // 2. 로그인 x - 로그인 페이지? or 현재 페이지? (Navigate 활용) - 논의 필요
+
   return (
     <header className={globalStyle.header}>
       <div>
@@ -11,9 +15,12 @@ const HeaderButton = () => {
         <span className={globalStyle.logoText}>Rolling</span>
       </div>
       <div>
-        <Button className={headerButtonStyle.createButton}>
-          롤링 페이퍼 만들기
-        </Button>
+        {/* Router로 감싸야 함 - main 컴포넌트에서 감쌌는데, merge 할 때는 어떻게 되는거지..? */}
+        <Link to="/post">
+          <Button className={headerButtonStyle.createButton}>
+            롤링 페이퍼 만들기
+          </Button>
+        </Link>
       </div>
     </header>
   );
--- src/Header/HeaderLogo.jsx
+++ src/Header/HeaderLogo.jsx
@@ -1,13 +1,20 @@
+import { Link } from "react-router-dom";
 import globalStyle from "../Header/global.module.css";
 import LogoImg from "../assets/header_logo_img.png";
 
 const HeaderLogo = () => {
   return (
     <header className={globalStyle.header}>
-      <div>
-        <img className={globalStyle.logoImg} src={LogoImg} alt="롤링페이퍼" />
-        <span className={globalStyle.logoText}>Rolling</span>
-      </div>
+      <Link to="/">
+        <div className={globalStyle.logoBox}>
+          <img
+            className={globalStyle.logoImg}
+            src={LogoImg}
+            alt="롤링페이퍼 로고"
+          />
+          <span className={globalStyle.logoText}>Rolling</span>
+        </div>
+      </Link>
     </header>
   );
 };
