2c227f0c758999bc4a40678912879bde09d672dd
limheewoo <heewoo39@gmail.com>
2025-07-21 10:57:33 +0900

    Feat: onClick disabled 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 //className prop안에 css 모듈 넣어서 사용하시면 됩니다!
-const Button = ({ className, children, onClick }) => {
+const Button = ({ className, disabled, onClick, children }) => {
   return (
-    <button className={className} onClick={onClick}>
+    <button className={className} disabled={disabled} onClick={onClick}>
       {children}
     </button>
   );
