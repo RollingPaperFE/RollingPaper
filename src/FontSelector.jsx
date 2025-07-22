ec19b751e145837adc538f23328ec3706883f199
aNN-algorithm <kyukyu0314@naver.com>
2025-07-22 12:20:31 +0900

    Feat: '폰트 선택' 옵션 추가

--- src/FontSelector.jsx
+++ src/FontSelector.jsx
@@ -6,6 +6,9 @@ const FontSelector = () => {
       <h2>폰트 선택</h2>
       <select>
         <option selected>Noto Sans</option>
+        <option>Pretendard</option>
+        <option>나눔명조</option>
+        <option>나눔손글씨 손편지체</option>
       </select>
     </div>
   );
