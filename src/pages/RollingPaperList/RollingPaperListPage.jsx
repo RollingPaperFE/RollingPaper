99094ed966486a1251af6fbf059052a58795ead4
limheewoo <heewoo39@gmail.com>
2025-07-23 15:11:13 +0900

    Feat: 특정 바디에 이미지 또는 색상 넣기

--- src/pages/RollingPaperList/RollingPaperListPage.jsx
+++ src/pages/RollingPaperList/RollingPaperListPage.jsx
@@ -3,12 +3,37 @@ import { useRollingPaper } from "./useRollingPaper";
 import RollingPaperCard from "./RollingPaperCard";
 import rollingListStyle from "./RollingPaperListPage.module.css";
 import HeaderContainer from "../../Header/HeaderApi";
+import { useEffect } from "react";
+import { useRecipientById } from "../../Header/HeaderIconBarContainer";
+
+const backgroundColorList = {
+  beige: "var(--beige-200)",
+  purple: "var(--purple-200)",
+  blue: "var(--blue-200)",
+  green: "var(--green-200)",
+};
 
 const RollingPaperListPage = () => {
   const { id } = useParams();
   const data = useRollingPaper(id);
   const { rollingPaper, error } = data;
   const { results } = rollingPaper;
+  const { recipients } = useRecipientById(id);
+  const { backgroundColor, backgroundImageURL } = recipients;
+
+  console.log(backgroundImageURL);
+  useEffect(() => {
+    if (backgroundImageURL) {
+      document.querySelector("body").style.backgroundImage = backgroundImageURL;
+    } else {
+      document.querySelector("body").style.background =
+        backgroundColorList[backgroundColor];
+    }
+
+    return () => {
+      document.querySelector("body").style.background = "white";
+    };
+  }, [backgroundColor, backgroundImageURL]);
 
   return (
     <>
