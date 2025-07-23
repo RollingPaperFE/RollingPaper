f133010bd902b56e84f5535b62fd3c034e621031
limheewoo <heewoo39@gmail.com>
2025-07-23 13:37:14 +0900

    Feat: 모달창 구현

--- src/pages/RollingPaperList/RollingPaperModal.jsx
+++ src/pages/RollingPaperList/RollingPaperModal.jsx
@@ -1,5 +1,6 @@
 import modalStyle from "./RollingPaperModal.module.css";
-import jisoo from "../../assets/headerAvatar/Jisoo.jpeg";
+import { changeDate } from "../../utils/changeDate";
+// import jisoo from "../../assets/headerAvatar/Jisoo.jpeg";
 
 const relationList = {
   가족: modalStyle["family"],
@@ -8,7 +9,9 @@ const relationList = {
   동료: modalStyle["coworker"],
 };
 
-const RollingPaperModal = ({ isOpen, handleModalClose }) => {
+const RollingPaperModal = ({ isOpen, handleModalClose, result }) => {
+  const { profileImageURL, sender, relationship, createdAt, content } = result;
+
   if (!isOpen) return null;
   return (
     <>
@@ -29,32 +32,26 @@ const RollingPaperModal = ({ isOpen, handleModalClose }) => {
             <img
               width={56}
               height={56}
-              src={jisoo}
+              src={profileImageURL}
               alt="img"
               className={modalStyle["sender-img"]}
             />
             <div className={modalStyle["sender-text-container"]}>
               <h3 className={modalStyle["sender"]}>
-                From. <b>김동훈</b>
+                From. <b>{sender}</b>
               </h3>
               <span
-                className={`${modalStyle["sender-relation"]} ${relationList["동료"]}`}
+                className={`${modalStyle["sender-relation"]} ${relationList[relationship]}`}
               >
-                동료
+                {relationship}
               </span>
             </div>
           </div>
-          <div className={modalStyle["card-create-date"]}>2023.12.12</div>
+          <div className={modalStyle["card-create-date"]}>
+            {changeDate(createdAt)}
+          </div>
         </div>
-        <div className={modalStyle["modal-content"]}>
-          코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또
-          하세요! 코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심
-          또 하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두
-          조심 또 하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력
-          모두 조심 또 하세요!코로나가 또다시 기승을 부리는 요즘이네요. 건강,
-          체력 모두 조심 또 하세요!코로나가 또다시 기승을 부리는 요즘이네요.
-          건강, 체력 모두 조심 또 하세요!
-        </div>
+        <div className={modalStyle["modal-content"]}>{content}</div>
         <button className={modalStyle["modal-btn"]} onClick={handleModalClose}>
           확인
         </button>
