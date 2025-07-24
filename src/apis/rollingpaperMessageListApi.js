3ee2bb240add331712adc7e3a0fb5fa2f23dd0a8
limheewoo <gmldn39@naver.com>
2025-07-24 10:53:41 +0900

    Fix: api수정

--- src/apis/rollingpaperMessageListApi.js
+++ src/apis/rollingpaperMessageListApi.js
@@ -8,7 +8,7 @@ const instance = axios.create({
 export const getRollingPaperMessage = async (params) => {
   const { id, offset } = params;
   const res = await instance
-    .get(`/recipients/${id}/messages/`, { offset })
+    .get(`/recipients/${id}/messages/`, { params: { offset } })
     .catch(function (error) {
       if (error.response) {
         // 요청이 전송되었고, 서버는 2xx 외의 상태 코드로 응답했습니다.
--- src/pages/RollingPaperList/useRollingPaper.jsx
+++ src/pages/RollingPaperList/useRollingPaper.jsx
@@ -10,11 +10,12 @@ export const useRollingPaper = (id) => {
     threshold: 0,
     delay: 100,
   });
+  const [hasMoreRollingPaper, setHasMoreRollingPaper] = useState(true);
 
   const handleLoad = useCallback(async () => {
     try {
       const { results, next } = await getRollingPaperMessage({ id, offset });
-      if (!next && offset !== 0) return;
+      if (!next && offset !== 0) setHasMoreRollingPaper(false);
 
       if (offset === 0) {
         setRollingPaper(results);
@@ -22,19 +23,17 @@ export const useRollingPaper = (id) => {
         setRollingPaper((prevResults) => [...prevResults, ...results]);
       }
 
-      if (next) {
-        setOffset((prevOffset) => prevOffset + 8);
-      }
+      if (next) setOffset((prevOffset) => prevOffset + 8);
     } catch (error) {
       setError(error);
     }
   }, [id, offset]);
 
   useEffect(() => {
-    if (inView) {
-      handleLoad();
-    }
-  }, [inView, handleLoad]);
+    if (!inView || !hasMoreRollingPaper) return;
+
+    handleLoad();
+  }, [inView, handleLoad, hasMoreRollingPaper]);
 
   return {
     rollingPaper,
