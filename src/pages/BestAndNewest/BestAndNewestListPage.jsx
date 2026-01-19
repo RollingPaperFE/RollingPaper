d47fc3b8352b206c160154fcfa572d47b082d7c6
limheewoo <gmldn39@naver.com>
2026-01-19 10:15:27 +0900

    Refactor: 데이터 받는 부분 수정

--- src/pages/BestAndNewest/BestAndNewestListPage.jsx
+++ src/pages/BestAndNewest/BestAndNewestListPage.jsx
@@ -2,14 +2,31 @@ import { Link } from "react-router-dom";
 import RecipientCardList from "./RecipientCardList";
 import bestAndNewestStyle from "./BestAndNewestListPage.module.css";
 import HeaderButton from "../../Header/HeaderButton";
+import { useAllRecipients } from "./useRecipient";
 
 const BestAndNewestListPage = () => {
+  const { bestData, newestData, error, handleMoveBest, handleMoveNewest } =
+    useAllRecipients();
   return (
     <>
       <HeaderButton isMake={true} isShow={true} />
       <div className={bestAndNewestStyle["list-page"]}>
-        <RecipientCardList title={"인기 롤링 페이퍼 🔥"} sort={"like"} />
-        <RecipientCardList title={"최근에 만든 롤링 페이퍼 ⭐️"} />
+        <RecipientCardList
+          title={"인기 롤링 페이퍼 🔥"}
+          recipients={bestData.recipients}
+          isLoading={bestData.isLoading}
+          error={error}
+          onMovePrevious={() => handleMoveBest("prev")}
+          onMoveNext={() => handleMoveBest("next")}
+        />
+        <RecipientCardList
+          title={"최근에 만든 롤링 페이퍼 ⭐️"}
+          recipients={newestData.recipients}
+          isLoading={newestData.isLoading}
+          error={error}
+          onMovePrevious={() => handleMoveNewest("prev")}
+          onMoveNext={() => handleMoveNewest("next")}
+        />
         <div className={bestAndNewestStyle["btn-container"]}>
           <Link to={"/post"} className={bestAndNewestStyle["create-btn"]}>
             나도 만들어보기
